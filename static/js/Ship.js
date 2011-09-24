d38e4688dd3c18bff5bbe90248b919f4ba0b60aa
Carlos Rodriguez <carlos@s8f.org>
2011-09-23 17:54:23 -0700

    Click to move.

--- static/js/Ship.js
+++ static/js/Ship.js
@@ -1,136 +1,50 @@
 /**
- * @author mikael emtinger / http://gomo.se/
- * @author alteredq / http://alteredqualia.com/
- *
- * parameters = {
- *  fov: <float>,
- *  aspect: <float>,
- *  near: <float>,
- *  far: <float>,
-
- *  movementSpeed: <float>,
- *  lookSpeed: <float>,
- *  rollSpeed: <float>,
-
- *  autoForward: <bool>,
- * 	mouseLook: <bool>,
-
- *  domElement: <HTMLElement>,
- * }
+ * @author carlos8f / http://s8f.org/
  */
 
 THREE.Ship = function ( geometry, material ) {
 
 	THREE.Mesh.call( this, geometry, material );
 
-	this.movementSpeed = 1;
+  this.travelLocation = new THREE.Vector3();
 
-  this.domElement = document;
+  this.speed = 0;
+  this.maxSpeed = 2;
 
-	// disable default camera behavior
-	this.useTarget = false;
-	this.matrixAutoUpdate = false;
+  var tmpDirection = new THREE.Vector3();
 
-	this.lastUpdate = -1;
-	this.delta = 0;
+  this.update = function( parentMatrixWorld, forceUpdate, camera ) {
 
-  this.thrust = 0;
-	var thrustDir = 0;
+    this.followTarget();
 
-	// custom update
-
-	this.update = function( parentMatrixWorld, forceUpdate, camera ) {
-
-		var now = new Date().getTime();
-
-		if ( this.lastUpdate == -1 ) this.lastUpdate = now;
-
-		this.delta = ( now - this.lastUpdate ) / 1000;
-		this.lastUpdate = now;
-
-		var actualSpeed = this.delta * this.movementSpeed;
-
-    var thrust = this.thrust;
-
-    switch ( thrustDir ) {
-
-      case 1: thrust += (thrust * 0.05) + 0.01; break;
-      case -1: thrust -= (thrust * 0.05) + 0.01; break;
-      case 0:
-        if ( thrust > 0 ) {
-
-          thrust -= (thrust * 0.05);
-
-        } else if ( thrust < 0 ) {
-
-          thrust += (- thrust * 0.05);
-
-        }
-        break;
-    }
-
-    this.thrust = constrain( thrust, -1, 1 );
-
-		
-
-		// call supr
+    // call supr
 		this.supr.update.call( this, parentMatrixWorld, forceUpdate, camera );
 
-	};
+  };
 
-  function constrain( scalar, min, max ) {
+  this.followTarget = function() {
 
-    if ( scalar > max ) {
+    tmpDirection.sub( this.travelLocation, this.position );
+    var length = tmpDirection.length();
 
-      return max;
+    if ( length > this.maxSpeed ) {
+
+      tmpDirection.normalize().multiplyScalar( this.maxSpeed );
+
+      this.position.addSelf( tmpDirection );
+    }
+    else {
+      
+      tmpDirection.normalize().multiplyScalar( length );
+      this.position.addSelf( tmpDirection );
 
     }
-    else if ( scalar < min ) {
 
-      return min;
+  };
 
-    }
-
-    return scalar;
-
-  }
-
-	function onKeyDown( event ) {
-
-		switch( event.keyCode ) {
-
-			case 38: /*up*/
-			case 87: /*W*/ thrustDir = 1; break;
-
-			case 40: /*down*/
-			case 83: /*S*/ thrustDir = -1; break;
-
-		}
-
-	};
-
-	function onKeyUp( event ) {
-
-		switch( event.keyCode ) {
-
-			case 38: /*up*/
-			case 87: /*W*/ thrustDir = 0; break;
-
-			case 40: /*down*/
-			case 83: /*S*/ thrustDir = 0; break;
-
-		}
-
-	};
-
-	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
-	this.domElement.addEventListener( 'keydown', onKeyDown, false );
-	this.domElement.addEventListener( 'keyup', onKeyUp, false );
 };
 
 
 THREE.Ship.prototype = new THREE.Mesh();
 THREE.Ship.prototype.constructor = THREE.Ship;
 THREE.Ship.prototype.supr = THREE.Mesh.prototype;
-
-
--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,23 @@
-
-var camera, scene, renderer, ship, stats;
+var camera, scene, renderer, stats, ship, projector, ray, plane, mouse2d, mouse3d;
 
 $(init);
 
 function init() {
 
   scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2( 0x000000, 0.0045 );
+  scene.fog = new THREE.FogExp2( 0xffffff, 0.0045 );
+
+  var cubeGeom, mesh;
 
   // grid
-  var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true, opacity: 0.9 } );
+  plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 100, 100 ), new THREE.MeshBasicMaterial( { color: 0x555555, opacity: 0.8, wireframe: true } ) );
+  scene.addChild( plane );
+
+  var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x888888, wireframe: true, opacity: 0.9 } );
   var distance = 1000;
   for ( var i = 0; i < distance; i++ ) {
-    var cubeGeom = new THREE.CubeGeometry( Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
-    var mesh = new THREE.Mesh( cubeGeom, cubeMaterial );
+    cubeGeom = new THREE.CubeGeometry( Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
+    mesh = new THREE.Mesh( cubeGeom, cubeMaterial );
     mesh.position.set(( Math.random() - 0.5 ) * distance,
     ( Math.random() - 0.5 ) * distance,
     0 );
@@ -25,11 +29,11 @@ function init() {
   }
 
   for ( var i = 0; i < distance; i++ ) {
-    var cubeGeom = new THREE.CubeGeometry( Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
-    var mesh = new THREE.Mesh( cubeGeom, cubeMaterial );
+    cubeGeom = new THREE.CubeGeometry( Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
+    mesh = new THREE.Mesh( cubeGeom, cubeMaterial );
     mesh.position.set(( Math.random() - 0.5 ) * distance,
     ( Math.random() - 0.5 ) * distance,
-    100 );
+    150 );
 
     mesh.updateMatrix();
     mesh.matrixAutoUpdate = false;
@@ -38,11 +42,11 @@ function init() {
   }
 
   for ( var i = 0; i < distance; i++ ) {
-    var cubeGeom = new THREE.CubeGeometry( Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
-    var mesh = new THREE.Mesh( cubeGeom, cubeMaterial );
+    cubeGeom = new THREE.CubeGeometry( Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
+    mesh = new THREE.Mesh( cubeGeom, cubeMaterial );
     mesh.position.set(( Math.random() - 0.5 ) * distance,
     ( Math.random() - 0.5 ) * distance,
-    -100 );
+    -150 );
 
     mesh.updateMatrix();
     mesh.matrixAutoUpdate = false;
@@ -66,6 +70,42 @@ function init() {
   var binLoader = new THREE.BinaryLoader();
   binLoader.load( { model: '/models/ship.js', callback: function( geometry ) { addModel( geometry ) } } );
 
+  projector = new THREE.Projector();
+  mouse2d = new THREE.Vector3();
+  mouse3d = new THREE.Vector3();
+  var windowHalfX = window.innerWidth / 2;
+	var windowHalfY = window.innerHeight / 2;
+
+  function constrain( scalar, min, max ) {
+
+    if ( scalar > max ) {
+      return max;
+    }
+    else if ( scalar < min ) {
+      return min;
+    }
+
+    return scalar;
+
+  }
+
+	function onMouseUp ( event ) {
+
+    if ( event.button == 2 ) {
+      mouse2d.set( (event.clientX - windowHalfX) / window.innerWidth, - ((event.clientY - windowHalfY) / window.innerHeight) ).multiplyScalar( 2 );
+      mouse3d = projector.unprojectVector( mouse2d.clone(), camera );
+      ray.direction = mouse3d.subSelf( camera.position ).normalize();
+      var intersects = ray.intersectObject( plane );
+      if ( intersects.length > 0 ) {
+        ship.travelLocation.copy( intersects[0].point );
+      }
+    }
+
+	};
+
+	document.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
+	document.addEventListener( 'mouseup', onMouseUp, false );
+
   function addModel ( geometry ) {
     
     ship = new THREE.Ship( geometry, new THREE.MeshFaceMaterial() );
@@ -74,12 +114,14 @@ function init() {
     ship.mouseLook = false;
     ship.rotation.x = Math.PI * 0.5;
     ship.matrixAutoUpdate = true;
+    ship.scale.set( 2, 2, 2 );
     scene.addChild( ship );
 
-    camera = new THREE.FollowCamera( 60, window.innerWidth / window.innerHeight, 1, 2000, ship, 100, 10, 200 );
+    camera = new THREE.FollowCamera( 60, window.innerWidth / window.innerHeight, 1, 2000, ship, 110, 10, 200 );
     ship.addChild( camera );
+    ray = new THREE.Ray( camera.position, null );
 
-    $('.info').html('Space demo.<br /><small>Controls: WASD w/ mouse<br /><a href="#" onclick="ship.reset();">reset ship</a> | <a href="#" onclick="ship.toggleMouseLook();">toggle mouseLook</a></small>');
+    $('.info').empty();
 
     stats = new Stats();
     stats.domElement.style.position = 'absolute';
@@ -93,16 +135,15 @@ function init() {
   // renderer
 
   renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setClearColorHex( 0x000000, 1 );
+  renderer.setClearColorHex( 0xffffff, 1 );
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   $('body').append( renderer.domElement );
 }
 
-
 function animate() {
   requestAnimationFrame(animate);
+
   renderer.render( scene, camera );
   stats.update();
 }
-
